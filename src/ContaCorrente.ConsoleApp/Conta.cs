726056d7c34cc38715c9d28f88d544ac4d05b9ad
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-04-11 20:06:35 -0300

    Commit do Projeto

---
+++ src/ContaCorrente.ConsoleApp/Conta.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrente.ConsoleApp
+{
+    internal class Conta
+    {
+        private int numero;
+        private double saldo, limite;
+        private bool ehEspecial;
+        private string[] historico = new string[10];
+        private Cliente titular;
+
+        private void RegistrarMovimentacao(double valor, string tipo)
+        {
+            for (int i = 0; i < historico.Length; i++)
+            {
+                if (historico[i] is null)
+                {
+                    historico[i] = $"Numero da Conta: {this.numero}\nValor da movimentação: {valor}" +
+                        $"\nTipo da Movimentação: {tipo}\nSaldo AtuaL: {saldo}\n";
+                    break;
+                }
+            }
+        }
+        public void CriarConta(int conta, double limite, bool ehEspecial, Cliente titular)
+        {
+            this.numero = conta;
+            this.saldo = 0;
+            this.limite = limite;
+            this.ehEspecial = ehEspecial;
+            this.titular = titular;
+
+        }
+        public void Sacar(double valor, bool ehTransferencia)
+        {
+            if (ehTransferencia)
+            {
+                if (valor < (this.limite + this.saldo))
+                {
+                    this.saldo -= valor;
+                    RegistrarMovimentacao(valor, "Transferencia");
+                }
+            }
+            else
+            {
+                if (valor < (this.limite + this.saldo))
+                {
+                    this.saldo -= valor;
+                    RegistrarMovimentacao(valor, "Saque");
+                }
+            }
+        }
+        public void Depositar(double valor, bool ehTransferencia)
+        {
+            if (ehTransferencia)
+            {
+                this.saldo += valor;
+                RegistrarMovimentacao(valor, "Transferencia");
+            }
+            else
+            {
+                this.saldo += valor;
+                RegistrarMovimentacao(valor, "Deposito");
+            }
+
+        }
+        public void VisualisarSaldo()
+        {
+            Console.WriteLine($"Titular da conta: {titular.nome} {titular.sobrenome}\nCPF: {titular.cpf}\n");
+            Console.WriteLine($"Conta Especial: {ehEspecial}");
+            Console.WriteLine($"Saldo da conta do {titular.nome}: {this.saldo}\n");
+        }
+        public void VisualizarExtrato()
+        {
+            Console.WriteLine($"Titular da conta: {titular.nome} {titular.sobrenome}\nCPF: {titular.cpf}\n");
+            for (int i = 0; i < historico.Length; i++)
+            {
+
+                if (historico[i] != null)
+                    Console.WriteLine(historico[i]);
+            }
+        }
+        public void Transferir(double valor, Conta conta)
+        {
+            Depositar(valor, true);
+            conta.Sacar(valor, true);
+        }
+    }
+}
--- src/ContaCorrente.ConsoleApp/Program.cs
+++ src/ContaCorrente.ConsoleApp/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Conta conta1 = new Conta();
+            Conta conta2 = new Conta();
+            Cliente titular1 = new Cliente();
+            Cliente titular2 = new Cliente();
+
+            titular1.nome = "Jonas";
+            titular1.sobrenome = "Correa";
+            titular1.cpf = "111.111.111-11";
+
+            titular2.nome = "Eduardo";
+            titular2.sobrenome = "Tortelli";
+            titular2.cpf = "222.222.222-22";
+
+            conta1.CriarConta(1, 1000, false, titular1);
+            conta2.CriarConta(2, 1000, false, titular2);
+
+            conta1.VisualisarSaldo();
+            conta1.Depositar(1000, false);
+            conta1.Sacar(500, false);
+            Console.WriteLine("Histórico");
+            conta1.VisualizarExtrato();
+
+            Console.WriteLine("------------------");
+
+            conta2.VisualisarSaldo();
+            conta2.Depositar(2000, false);
+            conta2.Sacar(500, false);
+            Console.WriteLine("Histórico");
+            conta2.VisualizarExtrato();
+
+            conta1.Transferir(100, conta2);
+
+            Console.WriteLine("------------------");
+            Console.WriteLine("Histórico");
+            conta1.VisualizarExtrato();
+            Console.WriteLine("------------------");
+            Console.WriteLine("Histórico");
+            conta2.VisualizarExtrato();
+
         }
     }
 }
